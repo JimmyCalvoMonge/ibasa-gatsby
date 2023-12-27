595a7b3e92a9de421aff1ca16eddbeef3658179c
jimmycalvo <jimjocamon94@gmail.com>
2023-12-27 12:35:56 -0600

    Updated CRM script 3

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -12,13 +12,6 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents}, plugin
   ])
 
   setPostBodyComponents([
-    <script
-      dangerouslySetInnerHTML={{
-          __html: `
-          !function(a,m,o,c,r,m){a[o+c]=a[o+c]||{setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1204064",hash:"7c735ce48b32369276eb686c785a8da1",locale:"es"}),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");
-      `,
-      }}
-      />,
     <script id="amoforms_script_1204064" async="async" 
     charset="utf-8" src="https://forms.kommo.com/forms/assets/js/amoforms.js?1702422060"></script>,
     <Footer></Footer>
